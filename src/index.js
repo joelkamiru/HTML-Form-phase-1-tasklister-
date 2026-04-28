8950fdd6c9cb9120d1546740f0da46a70b08ad7a
Joel Kamiru <kamirujoel2000@gmail.com>
2026-04-28 18:52:03 +0300

    completed the lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+ 
+  const taskForm = document.querySelector('#create-task-form');
+  const taskList = document.querySelector('#tasks');
+  const taskInput = document.querySelector('#new-task-description');
+
+  
+  taskForm.addEventListener('submit', (event) => {
+    
+    event.preventDefault();
+
+    const taskDescription = taskInput.value;
+
+    const newTaskItem = document.createElement('li');
+    newTaskItem.textContent = taskDescription;
+
+    taskList.append(newTaskItem);
+
+    taskForm.reset();
+  });
 });
